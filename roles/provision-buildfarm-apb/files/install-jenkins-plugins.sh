pb82/aerogear-digger-apb

commit 4405c5522a84b50bd0f99725f22c12b452b5add0

AEROGEAR-2574 Update the APB to add a de-provision phase

roles/provision-buildfarm-apb/files/install-jenkins-plugins.sh
@@ -1,24 +1,24 @@
-# $1 - Name of the Deployment Config
-# $2 - Number of availableReplicas to wait for
-# $3 - Namespace
+#!/usr/bin/env bash
 function wait_for_scale {
-  for i in {0..100}; do
-    sleep 5
-    echo "oc get dc ${1} -n ${3} -o jsonpath='{.status.availableReplicas}'"
-    if [ $(oc get dc "${1}" -n "${3}" -o jsonpath="{.status.availableReplicas}") == "${2}" ]; then
-      return
-    fi
-    echo "Waiting for ${1} to scale to ${2}, currently $(oc get dc ${1} -n ${3} -o jsonpath='{.status.availableReplicas}')"
+  local dc="${1:?"[ERROR]You must provide a deployment configuration."}"
+  local rc="${2:?"[ERROR]You must provide a number of replicas"}"
+  local ns="${3:?"[ERROR]You must provide a namespace."}"
+
+  local var=$(oc get dc "${dc}" -n "${ns}" -o jsonpath="{.status.availableReplicas}")
+  while [ "${var}" -lt "${rc}" ]
+  do
+    echo "Waiting for ${dc} to scale to ${rc}, currently ${var}"; sleep 5
+    var=$(oc get dc "${dc}" -n "${ns}" -o jsonpath="{.status.availableReplicas}")
   done
 }
 
 echo "Waiting for Jenkins to be available"
 
-readonly namespace="${1}"
+readonly namespace="${1:?"[ERROR]You must provide a namespace."}"
 
-wait_for_scale "jenkins" 1 "${namespace}"
+wait_for_scale "{{ ag_jenkins.deployment.name }}" 1 "${namespace}"
 
-readonly jenkins_pod=$(oc get pods -n ${1} | grep jenkins- | awk '{ print $1 }')
+readonly jenkins_pod=$(oc get pods -n ${namespace} | grep "{{ ag_jenkins.deployment.name }}"- | awk '{ print $1 }')
 
 echo "Copying Jenkins plugins from /jenkins-plugins/"
 
@@ -27,10 +27,10 @@ oc cp -n ${namespace} /tmp/jenkins-plugins/android-signing.jpi ${jenkins_pod}:/v
 
 echo "Scaling down Jenkins"
 
-oc scale dc jenkins --replicas=0 -n ${namespace}
-wait_for_scale "jenkins" 0 ${namespace}
+oc scale dc/"{{ ag_jenkins.deployment.name }}" --replicas=0 -n ${namespace}
+wait_for_scale "{{ ag_jenkins.deployment.name }}" 0 ${namespace}
 
 echo "Scaling up Jenkins"
 
-oc scale dc jenkins --replicas=1 -n ${namespace}
-wait_for_scale "jenkins" 1 ${namespace}
+oc scale dc/"{{ ag_jenkins.deployment.name }}" --replicas=1 -n ${namespace}
+wait_for_scale "{{ ag_jenkins.deployment.name }}" 1 ${namespace}

roles/provision-buildfarm-apb/tasks/main.yml
@@ -1,26 +1,4 @@
-##############################################################################
-## Provision buildfarm
-## This role executes much of the needed functionality to provision an
-## application using an Ansible Playbook Bundle.  Included in the comments
-## below are some sample resources for getting started deploying an application
-## to OpenShift.
-##############################################################################
-
-- name: "Include variables"
-  include_vars: vars/main.yml
-  tags:
-  - deploy-sdk-pod
-
-- name: Get the name of the service instance
-  shell: oc get serviceinstance --namespace={{ namespace }} -o jsonpath='{.items[?(@.spec.externalID=="{{ _apb_service_instance_id }}")].metadata.name}'
-  when: _apb_service_instance_id is defined
-  register: service_instance_name
-
-- name: Label the service instance with the service name
-  shell: oc label serviceinstance '{{ service_instance_name.stdout }}' serviceName=aerogear-digger --namespace={{ namespace }}
-  when: _apb_service_instance_id is defined
-
-- name: Create Mobile CI/CD secret file
+- name: Create Mobile CI|CD secret file
   template:
     src: aerogear-digger.secret.yaml.j2
     dest: /tmp/aerogear-digger.secret.yaml
@@ -30,11 +8,11 @@
     jenkinsUri: '""'
     jenkinsPreexists: "false"
 
-- name: Create Mobile CI/CD secret
+- name: Create Mobile CI|CD secret
   command: "oc create -f /tmp/aerogear-digger.secret.yaml -n {{ namespace }}"
 
 - name: Check for current Jenkins
-  command: "oc get service jenkins -n {{ namespace }}"
+  command: "oc get service {{ ag_jenkins.service.name }} -n {{ namespace }}"
   failed_when: false
   changed_when: false
   register: jenkins_check
@@ -44,26 +22,18 @@
   when: jenkins_check.rc != 0
 
 - set_fact:
-    jenkins_service_preexists: "true"
-  when: jenkins_check.rc == 0
-
-- set_fact:
-    jenkins_service_preexists: "false"
-  when: jenkins_check.rc != 0
+    jenkins_service_preexists: "{{ 'true' if jenkins_check.rc == 0 else 'false' }}"
 
 - name: Copy Jenkins plugin install script to host
   copy:
-    src: install-jenkins-plugins.sh
-    dest: /tmp/install-jenkins-plugins.sh
+    src: '{{ item.src }}'
+    dest: '{{ item.dest }}'
     force: yes
+  with_items:
+    - {src: install-jenkins-plugins.sh, dest: /tmp/install-jenkins-plugins.sh}
+    - {src: plugins/, dest: /tmp/jenkins-plugins/}
 
-- name: Copy Jenkins plugin binaries to host
-  copy:
-    src: plugins/
-    dest: /tmp/jenkins-plugins/
-    force: yes
-
-- include: update-secret.yml
+- include_tasks: update-secret.yml
   vars:
     status_message: Installing Jenkins plugins
     status_percentage: "20"
@@ -74,8 +44,8 @@
   shell: "bash /tmp/install-jenkins-plugins.sh {{ namespace }}"
   register: install_jenkins_plugins
 
-- name: Get Jenkins - Mobile CI/CD URI
-  shell: "oc get routes jenkins -n {{ namespace}}  | grep -v NAME | awk '{ print $2 }'"
+- name: Get Jenkins - Mobile CI|CD URI
+  shell: "oc get routes {{ ag_jenkins.route.name }} -n {{ namespace}}  | grep -v NAME | awk '{ print $2 }'"
   register: get_jenkins_uri
 
 - set_fact:
@@ -104,7 +74,7 @@
 
 - name: "Update local android-sdk-persistent template with image version and tag"
   template:
-    src: android-sdk-persistent-template.j2
+    src: android-sdk-persistent-template.json.j2
     dest: "{{ buildfarm_templates_dir }}/android-sdk-persistent-template.json"
   tags:
   - deploy-sdk-pod
@@ -128,7 +98,7 @@
   local_action: stat path=../playbooks/tasks/poller.yml
   register: poller
 
-- include: update-secret.yml
+- include_tasks: update-secret.yml
   vars:
     status_message: Creating Android SDK container
     status_percentage: "30"
@@ -137,7 +107,7 @@
 
 - block:
   - name: "Poll for Pods to become ready"
-    command: "oc get dc android-sdk --output jsonpath='{.status.availableReplicas}' -n {{ namespace }}"
+    command: "oc get dc/{{ ag_android_sdk.deployment.name }} --output jsonpath='{.status.availableReplicas}' -n {{ namespace }}"
     register: poll_result
     until: poll_result.rc == 0 and poll_result.stdout | int > 0
     retries: 60
@@ -146,18 +116,18 @@
     - deploy-sdk-pod
   rescue:
   - name: "Attempting redeploy of failed Pod"
-    command: "oc deploy --retry dc/android-sdk -n {{ namespace }}"
+    command: "oc deploy --retry dc/{{ ag_android_sdk.deployment.name }} -n {{ namespace }}"
   when: not poller.stat.exists
 
 - name: "Get the Android SDK pod name"
-  shell: "oc get pods -n {{ namespace }} | grep android-sdk | grep -v deploy | awk '{print$1}'"
+  shell: "oc get pods -n {{ namespace }} | grep {{ ag_android_sdk.deployment.name }} | grep -v deploy | awk '{print$1}'"
   failed_when: output.stdout == ""
   register: output
   changed_when: False
   tags:
   - install-sdk
 
-- name: "Register android-sdk pod name"
+- name: "Register {{ ag_android_sdk.deployment.name }} pod name"
   set_fact:
     android_sdk_podname: "{{ output.stdout }}"
   tags:
@@ -185,7 +155,7 @@
   tags:
     - install-sdk
 
-- include: update-secret.yml
+- include_tasks: update-secret.yml
   vars:
     status_message: Configuring Android SDK container
     status_percentage: "40"
@@ -228,7 +198,7 @@
   tags:
   - install-sdk
 
-- include: update-secret.yml
+- include_tasks: update-secret.yml
   vars:
     status_message: Configuring Gradle
     status_percentage: "80"
@@ -269,7 +239,7 @@
   tags:
   - install-sdk
 
-- include: update-secret.yml
+- include_tasks: update-secret.yml
   vars:
     status_message: Complete
     status_percentage: "100"

roles/deprovision-buildfarm-apb/tasks/main.yml
@@ -1,15 +1,55 @@
-- name: delete AG-Digger resoures
-  command: "oc delete {{ item }} android-sdk -n {{ namespace }}"
+---
+- name: Delete AG-Jenkins route
+  openshift_v1_route:
+    name: '{{ item }}'
+    namespace: '{{ namespace }}'
+    state: absent
   with_items:
-    - dc
+  - '{{ ag_jenkins.route.name }}'
 
-- name: delete AG-Digger secret
-  command: "oc delete {{ item }} aerogear-digger -n {{ namespace }}"
+- name: Delete AG-Jenkins services
+  k8s_v1_service:
+    name: '{{ item }}'
+    namespace: '{{ namespace }}'
+    state: absent
   with_items:
-    - secret
+  - '{{ ag_jenkins.service.name }}'
+  - '{{ ag_jenkins.service.jnlp }}'
+
+- name: Delete AG-Digger Deployment Configs
+  command: "oc delete dc/{{ item }} -n {{ namespace }}"
+  with_items:
+  - '{{ ag_android_sdk.deployment.name }}'
+  - '{{ ag_jenkins.deployment.name }}'
+
+- name: Delete AG-Digger Service Accounts
+  command: "oc delete serviceaccounts/{{ item }} -n {{ namespace }}"
+  with_items:
+  - '{{ ag_android_sdk.service.name }}'
+  - '{{ ag_jenkins.service.name }}'
+
+- name: Delete AG-Digger Role Bindings
+  command: "oc delete rolebindings/{{ item }} -n {{ namespace }}"
+  with_items:
+  - '{{ ag_android_sdk.rolebinding.name }}'
+  - '{{ ag_jenkins.rolebinding.name }}'
+
+- name: Delete AG-Digger secrets
+  command: "oc delete secrets/{{ item }} -n {{ namespace }}"
+  with_items:
+  - '{{ ag_digger_secret_name }}'
 
 - name: delete AG-Digger configmaps
-  command: "oc delete configmap {{ item }} -n {{ namespace }}"
+  command: "oc delete configmaps/{{ item }} -n {{ namespace }}"
   with_items:
-    - android
-    - android-proxy
+  - '{{ ag_android_sdk.configmap.name }}'
+  - '{{ ag_android_sdk.configmap.proxy }}'
+
+- name: Delete AG-Digger PV claims
+  k8s_v1_persistent_volume_claim:
+    name: '{{ item }}'
+    namespace: '{{ namespace }}'
+    state: absent
+  with_items:
+  - '{{ ag_android_sdk.pv_claim.name }}'
+  - '{{ ag_jenkins.pv_claim.name }}'

roles/provision-buildfarm-apb/defaults/main.yml
@@ -1,7 +1,10 @@
 ---
-android_sdk_home: /opt/android-sdk-linux
+android_sdk_home: "{{ lookup('env', 'ANDROID_HOME')|default('/opt/android-sdk-linux', True) }}"
 android_slave_image: "docker.io/aerogear/digger-android-slave-image:latest"
 android_slave_instance_cap: 3
 android_slave_idle_minutes: 3
+
+java_sdk_home: "{{ lookup('env', 'JAVA_HOME')|default('/etc/alternatives/java_sdk_1.8.0', True) }}"
+
 jenkins_master_memory_limit: 3Gi
 jenkins_master_volume_capacity: 40Gi

roles/provision-buildfarm-apb/vars/main.yml
@@ -59,7 +59,7 @@ buildfarm:
    pause_for: 20
 # default images for the Android slave containers. You don't need to change them.
 deployments:
-  - name: "android-sdk"
+  - name: "{{ ag_android_sdk.deployment.name }}"
     containers:
     - name: "android-sdk"
       image: "docker.io/aerogear/digger-android-sdk-image:latest"

vars/main.yml
@@ -1,0 +1,28 @@
+---
+ag_jenkins:
+  route:
+    name: 'jenkins'
+  service:
+    name: 'jenkins'
+    jnlp: 'jenkins-jnlp'
+  rolebinding:
+    name: 'jenkins_edit'
+  deployment:
+    name: 'jenkins'
+  pv_claim:
+    name: 'jenkins'
+
+ag_android_sdk:
+  service:
+    name: 'android-sdk'
+  rolebinding:
+    name: 'android-sdk_edit'
+  deployment:
+    name: 'android-sdk'
+  configmap:
+    name: 'android'
+    proxy: 'android-proxy'
+  pv_claim:
+    name: 'android-sdk'
+
+ag_digger_secret_name: 'aerogear-digger'

.gitignore
@@ -1,0 +1,1 @@
+.idea/

Dockerfile
@@ -3,29 +3,30 @@ FROM ansibleplaybookbundle/apb-base
 LABEL "com.redhat.apb.version"="0.1.0"
 LABEL "com.redhat.apb.spec"=\
 "dmVyc2lvbjogMS4wCm5hbWU6IGFlcm9nZWFyLWRpZ2dlci1hcGIKZGVzY3JpcHRpb246IE1vYmls\
-ZSBDSS9DRCBzZXJ2aWNlIHRoYXQgaW50ZWdyYXRlcyB3aXRoIEplbmtpbnMgUGlwZWxpbmVzIHRv\
+ZSBDSXxDRCBzZXJ2aWNlIHRoYXQgaW50ZWdyYXRlcyB3aXRoIEplbmtpbnMgUGlwZWxpbmVzIHRv\
 IHRlc3QgYW5kIGJ1aWxkIHlvdXIgbW9iaWxlIGJpbmFyaWVzLgpiaW5kYWJsZTogRmFsc2UKYXN5\
-bmM6IG9wdGlvbmFsCnRhZ3M6CiAgLSBtb2JpbGUtc2VydmljZQptZXRhZGF0YToKICBkaXNwbGF5\
-TmFtZTogTW9iaWxlIENJL0NECiAgbG9uZ0Rlc2NyaXB0aW9uOiAiVG8gdXNlIHRoZSBNb2JpbGUg\
-Q0kvQ0Qgc2VydmljZSB5b3UgbXVzdCBmaXJzdCBhZ3JlZSB0byB0aGUgQW5kcm9pZCBTREsgTGlj\
-ZW5zZSBBZ3JlZW1lbnQuIEJ5IGNoZWNraW5nIHRoaXMgYm94IHlvdSBhcmUgYWdyZWVpbmcgdG8g\
-dGhlIEFuZHJvaWQgU29mdHdhcmUgRGV2ZWxvcG1lbnQgS2l0IExpY2Vuc2UgYWdyZWVtZW50IHdo\
-aWNoIGNhbiBiZSByZWFkIGF0IHRoZSBmb2xsb3dpbmcgVVJMOiBodHRwczovL2RldmVsb3Blci5h\
-bmRyb2lkLmNvbS9zdHVkaW8vdGVybXMuaHRtbFxuXG4uIElmIHRoZSBib3ggaXMgbm90IGNoZWNr\
-ZWQgdGhlIGluc3RhbGxlciB3aWxsIHRha2Ugbm8gYWN0aW9uLiIKICBzZXJ2aWNlTmFtZTogYWVy\
-b2dlYXItZGlnZ2VyCiAgaW1hZ2VVcmw6ICJodHRwczovL2Flcm9nZWFyLm9yZy9pbWcvYWVyb2dl\
-YXJkaWdnZXJfaWNvbl8zMnB4X2Nyb3BwZWQucG5nIgogIGRvY3VtZW50YXRpb25Vcmw6ICJodHRw\
-czovL2Flcm9nZWFyLm9yZy9kaWdnZXIvIgogIHByb3ZpZGVyRGlzcGxheU5hbWU6ICJSZWQgSGF0\
-LCBJbmMuIgpwbGFuczoKICAtIG5hbWU6IGRlZmF1bHQKICAgIGRlc2NyaXB0aW9uOiBEZWZhdWx0\
-IHBsYW4KICAgIGZyZWU6IFRydWUKICAgIG1ldGFkYXRhOiB7fQogICAgcGFyYW1ldGVyczoKICAg\
-IC0gbmFtZTogQU5EUk9JRF9MSUNFTlNFX0FHUkVFTUVOVAogICAgICBkZWZhdWx0OiAiIgogICAg\
-ICBwYXR0ZXJuOiBeeWVzJAogICAgICB0eXBlOiBzdHJpbmcKICAgICAgZGVzY3JpcHRpb246ICJU\
-eXBlICd5ZXMnIGFib3ZlIHRvIGFjY2VwdCB0aGUgQW5kcm9pZCBTREsgTGljZW5zZSBBZ3JlZW1l\
-bnQuIGh0dHBzOi8vZGV2ZWxvcGVyLmFuZHJvaWQuY29tL3N0dWRpby90ZXJtcy5odG1sIgogICAg\
-ICB0aXRsZTogQW5kcm9pZCBTREsgTGljZW5zZSBBZ3JlZW1lbnQKICAgICAgcmVxdWlyZWQ6IFRy\
-dWUK"
+bmM6IG9wdGlvbmFsCnRhZ3M6CiAgLSBtb2JpbGUtc2VydmljZQogIC0gbW9iaWxlLWNsaWVudC1l\
+bmFibGVkCm1ldGFkYXRhOgogIGRpc3BsYXlOYW1lOiBNb2JpbGUgQ0l8Q0QKICBsb25nRGVzY3Jp\
+cHRpb246ICJUbyB1c2UgdGhlIE1vYmlsZSBDSXxDRCBzZXJ2aWNlIHlvdSBtdXN0IGZpcnN0IGFn\
+cmVlIHRvIHRoZSBBbmRyb2lkIFNESyBMaWNlbnNlIEFncmVlbWVudC4gQnkgY2hlY2tpbmcgdGhp\
+cyBib3ggeW91IGFyZSBhZ3JlZWluZyB0byB0aGUgQW5kcm9pZCBTb2Z0d2FyZSBEZXZlbG9wbWVu\
+dCBLaXQgTGljZW5zZSBhZ3JlZW1lbnQgd2hpY2ggY2FuIGJlIHJlYWQgYXQgdGhlIGZvbGxvd2lu\
+ZyBVUkw6IGh0dHBzOi8vZGV2ZWxvcGVyLmFuZHJvaWQuY29tL3N0dWRpby90ZXJtcy5odG1sXG5c\
+bi4gSWYgdGhlIGJveCBpcyBub3QgY2hlY2tlZCB0aGUgaW5zdGFsbGVyIHdpbGwgdGFrZSBubyBh\
+Y3Rpb24uIgogIHNlcnZpY2VOYW1lOiBhZXJvZ2Vhci1kaWdnZXIKICBpbWFnZVVybDogImh0dHBz\
+Oi8vYWVyb2dlYXIub3JnL2ltZy9hZXJvZ2VhcmRpZ2dlcl9pY29uXzMycHhfY3JvcHBlZC5wbmci\
+CiAgZG9jdW1lbnRhdGlvblVybDogImh0dHBzOi8vYWVyb2dlYXIub3JnL2RpZ2dlci8iCiAgcHJv\
+dmlkZXJEaXNwbGF5TmFtZTogIlJlZCBIYXQsIEluYy4iCnBsYW5zOgogIC0gbmFtZTogZGVmYXVs\
+dAogICAgZGVzY3JpcHRpb246IERlZmF1bHQgcGxhbgogICAgZnJlZTogVHJ1ZQogICAgbWV0YWRh\
+dGE6IHt9CiAgICBwYXJhbWV0ZXJzOgogICAgLSBuYW1lOiBBTkRST0lEX0xJQ0VOU0VfQUdSRUVN\
+RU5UCiAgICAgIGRlZmF1bHQ6ICIiCiAgICAgIHBhdHRlcm46IF55ZXMkCiAgICAgIHR5cGU6IHN0\
+cmluZwogICAgICBkZXNjcmlwdGlvbjogIlR5cGUgJ3llcycgYWJvdmUgdG8gYWNjZXB0IHRoZSBB\
+bmRyb2lkIFNESyBMaWNlbnNlIEFncmVlbWVudC4gaHR0cHM6Ly9kZXZlbG9wZXIuYW5kcm9pZC5j\
+b20vc3R1ZGlvL3Rlcm1zLmh0bWwiCiAgICAgIHRpdGxlOiBBbmRyb2lkIFNESyBMaWNlbnNlIEFn\
+cmVlbWVudAogICAgICByZXF1aXJlZDogVHJ1ZQo="
 
 COPY playbooks /opt/apb/actions
 COPY roles /opt/ansible/roles
+COPY vars /opt/ansible/vars
 RUN chmod -R g=u /opt/{ansible,apb}
 USER apb

commit b51afe371ebd93e12b5e9fdf10842451eccea958

Pass jenkins deployment name into install plugins script. Fixes an issue introduced in https://github.com/aerogearcatalog/aerogear-digger-apb/pull/7

roles/provision-buildfarm-apb/files/install-jenkins-plugins.sh
@@ -15,10 +15,11 @@ function wait_for_scale {
 echo "Waiting for Jenkins to be available"
 
 readonly namespace="${1:?"[ERROR]You must provide a namespace."}"
+readonly jenkins_name="${2:?"[ERROR]You must provide an jenkins deployment name."}"
 
-wait_for_scale "{{ ag_jenkins.deployment.name }}" 1 "${namespace}"
+wait_for_scale ${jenkins_name} 1 ${namespace}
 
-readonly jenkins_pod=$(oc get pods -n ${namespace} | grep "{{ ag_jenkins.deployment.name }}"- | awk '{ print $1 }')
+readonly jenkins_pod=$(oc get pods -n ${namespace} | grep ${jenkins_name}- | awk '{ print $1 }')
 
 echo "Copying Jenkins plugins from /jenkins-plugins/"
 
@@ -29,10 +30,10 @@ oc cp -n ${namespace} /tmp/jenkins-plugins/openshift-sync.hpi ${jenkins_pod}:/va
 
 echo "Scaling down Jenkins"
 
-oc scale dc/"{{ ag_jenkins.deployment.name }}" --replicas=0 -n ${namespace}
-wait_for_scale "{{ ag_jenkins.deployment.name }}" 0 ${namespace}
+oc scale dc/${jenkins_name} --replicas=0 -n ${namespace}
+wait_for_scale ${jenkins_name} 0 ${namespace}
 
 echo "Scaling up Jenkins"
 
-oc scale dc/"{{ ag_jenkins.deployment.name }}" --replicas=1 -n ${namespace}
-wait_for_scale "{{ ag_jenkins.deployment.name }}" 1 ${namespace}
+oc scale dc/${jenkins_name} --replicas=1 -n ${namespace}
+wait_for_scale ${jenkins_name} 1 ${namespace}

roles/provision-buildfarm-apb/tasks/main.yml
@@ -41,7 +41,7 @@
     jenkins_preexists: "{{ jenkins_service_preexists }}"
 
 - name: Install Jenkins plugins
-  shell: "bash /tmp/install-jenkins-plugins.sh {{ namespace }}"
+  shell: "bash /tmp/install-jenkins-plugins.sh {{ namespace }} {{ ag_jenkins.deployment.name }}"
   register: install_jenkins_plugins
 
 - name: Get Jenkins - Mobile CI|CD URI